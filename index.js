4b8a8306046822cfe645c3409427e31a226cf484
周浩 <hzzhouhao1@corp.netease.com>
2016-08-09 18:43:48 +0800

    调整目录

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-module.exports = require("./src/init");
+module.exports = require("./lib/init");
---
+++ tests/swig.test.js
@@ -0,0 +1,9 @@
+const swig = require("swig");
+
+let template = swig.compileFile(__dirname + "/templates/index.html");
+let output = template({
+    pagename: 'awesome people',
+    authors: ['Paul', 'Jim', 'Jane']
+});
+
+console.log(output)
