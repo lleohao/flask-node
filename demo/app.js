444de36921febc75dfc3e34453f6357c84c880c4
lleohao <lleohao@hotmail.com>
2016-08-09 22:44:35 +0800

    init demo project

--- demo/app.js
+++ demo/app.js
@@ -1,3 +1,19 @@
+/// <reference path="../typings/index.d.ts" />
 const Flask = require("../");
 
-console.log(Flask);
+let app = new Flask(__dirname);
+let mainRoute = app.createRouter();
+let render = app.createRender();
+
+mainRoute.add("/", function(data, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(render("index.html"));
+});
+
+mainRoute.add("/upload", ["GET", "POST"], function(data, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end("/upload");
+});
+
+
+app.run({port: 8000,debug: true});
