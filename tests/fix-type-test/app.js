264e03d78a18dfba72789a72d45c654e6ffe9781
lleohao <lleohao@hotmail.com>
2016-08-13 11:40:14 +0800

    add test

---
+++ tests/fix-type-test/app.js
@@ -0,0 +1,16 @@
+/// <reference path="../../typings/index.d.ts" />
+const Flask = require('../../index');
+
+let app = new Flask(__dirname);
+let mainRouter = app.createRouter();
+let render = app.createRender();
+
+
+mainRouter.add("/", ["GET", "POST"], function (request, response) {
+    console.log(request);
+    response.setHeader("Set-Cookie", ["username=lleohao", "age=21"]);
+    response.writeHead("200", { 'Content-Type': 'text/html' });
+    response.end(render("index.html"));
+})
+
+app.run(debug = true);
