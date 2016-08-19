f13bcd2a9991d9e5bfcda66eb6d049209f94d6ba
周浩 <hzzhouhao1@corp.netease.com>
2016-08-19 11:42:58 +0800

    add debug log

--- tests/form-test/request.form.test.js
+++ tests/form-test/request.form.test.js
@@ -7,9 +7,10 @@ let render = app.createRender();
 
 
 mainRouter.add("/", ["GET", "POST"], function (request, response) {
+    console.log(request);
     response.setHeader("Set-Cookie", ["username=lleohao", "age=21"]);
     response.writeHead("200", { 'Content-Type': 'text/html' });
     response.end(render("form.html"));
 })
 
-app.run(debug = true);
+app.run({debug: true});
