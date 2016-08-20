bff2a6acb599c54cc90aecfd08eeb3acb7000133
周浩 <hzzhouhao1@corp.netease.com>
2016-08-20 19:31:06 +0800

    test-1 response

--- tests/response-test/app.js
+++ tests/response-test/app.js
@@ -5,10 +5,8 @@ const mainRouter = app.createRouter();
 const render = app.createRender();
 
 mainRouter.add('/', (req, res) => {
-
-
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(render('index.html', {
+    res.$res.writeHead(200, {'Content-Type': 'text/html'});
+    res.$res.end(render('index.html', {
         message: 'hello world'
     }));
 });
