307c432ae3fb341b9e8c51c53ed8c8764d73d94a
周浩 <hzzhouhao1@corp.netease.com>
2016-08-20 20:00:25 +0800

    delete old tests

--- tests/swig-test/swig.test.js
+++
@@ -1,7 +0,0 @@
-const swig = require("swig");
-
-let template = swig.compileFile(__dirname + "/templates/index.html");
-let output = template({
-    pagename: 'awesome people',
-    authors: ['Paul', 'Jim', 'Jane']
-});
