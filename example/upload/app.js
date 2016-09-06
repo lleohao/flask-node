4a7e335073f0b5e47599e1752902e2181133c599
周浩 <hzzhouhao1@corp.netease.com>
2016-09-06 15:32:44 +0800

    add upload demo

--- example/upload/app.js
+++ example/upload/app.js
@@ -4,7 +4,7 @@ const Router = require('../../lib').Router;
 const fs = require('fs');
 const path = require('path');
 
-let app = new Flask(__dirname);
+let app = new Flask(__dirname, {static: 'asset'});
 let mainRoute = new Router();
 
 mainRoute.add("/", function index(req, res) {
@@ -16,36 +16,33 @@ mainRoute.add("/view", function viewImg(req, res) {
 });
 
 mainRoute.add("/upload", ["POST"], function upload(req, res) {
-    // let _uploadFile = req.form.files.uploadFile;
-    // let _extname = _uploadFile.name.split(".")[1];
-    // let _tmpPath = _uploadFile.path;
-    // let _movePath = path.join(__dirname, 'static/img', 'upload.' + _extname);
-    // fs.exists(_movePath, function (exists) {
-    //     if (exists) {
-    //         fs.unlink(_movePath, function () {
-    //             fs.rename(_tmpPath, _movePath, function (err) {
-    //                 if (err) {
-    //                     console.log(err);
-    //                     response.writeHead(500, { 'Content-Type': 'text/plain' });
-    //                     response.end("The server error");
-    //                 } else {
-    //                     response.writeHead(200, { 'Content-Type': 'text/plain' });
-    //                     response.end("true");
-    //                 }
-    //             })
-    //         })
-    //     } else {
-    //         fs.rename(_tmpPath, _movePath, function (err) {
-    //             if (err) {
-    //                 console.log(err);
-    //                 response.writeHead(500, { 'Content-Type': 'text/plain' });
-    //                 response.end("The server error");
-    //             } else {
-    //                 res.redirect('/view');
-    //             }
-    //         })
-    //     }
-    // })
+    let _uploadFile = req.form.files.img;
+    let _extname = _uploadFile.name.split(".")[1];
+    let _tmpPath = _uploadFile.path;
+    let _movePath = path.join(__dirname, 'asset/img', 'upload.' + _extname);
+    fs.exists(_movePath, function (exists) {
+        if (exists) {
+            fs.unlink(_movePath, function () {
+                fs.rename(_tmpPath, _movePath, function (err) {
+                    if (err) {
+                        console.log(err);
+                        res.abort(500);
+                    } else {
+                        res.redirect('/view');
+                    }
+                })
+            })
+        } else {
+            fs.rename(_tmpPath, _movePath, function (err) {
+                if (err) {
+                    console.log(err);
+                    res.abort(500);
+                } else {
+                    res.redirect('/view');
+                }
+            })
+        }
+    })
 });
 
 app.run({debug: true});
