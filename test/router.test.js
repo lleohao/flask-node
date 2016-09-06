5dad794c6ce7920dec18aad934d00637e8db9b80
周浩 <hzzhouhao1@corp.netease.com>
2016-09-06 14:23:55 +0800

    添加测试

--- test/router.test.js
+++ test/router.test.js
@@ -1,5 +1,6 @@
 const Router = require('../lib/router');
 const handleRouter = Router.handleRouter;
+const testTools = Router.testTools;
 const should = require('should');
 
 describe('Router test', function () {
@@ -14,9 +15,15 @@ describe('Router test', function () {
 
     describe('add function test', function () {
         var router = new Router();
+        testTools();
+
         var callback = function callback() {
 
         };
+
+        it('no methods', function () {
+            router.add('/', callback);
+        });
 
         it('params path is error', function () {
             should.throws(function () {
@@ -24,32 +31,65 @@ describe('Router test', function () {
             });
         });
 
-        it('no methods', function () {
-            router.add('/', callback);
-        });
-
-        it('anonymous function', function () {
-            should.throws(function () {
-                router.add('/', function () {
-                });
-            });
-        })
     });
 
     describe('update function test: route is sort by _weight', function () {
         var router = new Router();
         var callback = function callback() {
+
         };
+
+        testTools();
         router.add('/post/<path:str>', callback);
         router.add('/post/<str:title>', callback);
         router.add('/post', callback);
         router.add('/', callback);
 
-        // console.log(Router.urlMap);
+        // ok;
     });
 
-    describe('handleRouter test', function () {
+    describe('dynamic router test', function () {
         var router = new Router();
 
+        it('static router /', function (done) {
+            testTools();
+            router.add('/', ['get'], function (req, res) {
+                console.log('req: ', req);
+                req.pathname.should.be.eql('/');
+                done();
+            });
+
+            handleRouter({ pathname: '/' }, {});
+        });
+
+        it('static router /post', function (done) {
+            testTools();
+            router.add('/post', ['get'], function (req, res) {
+                req.pathname.should.be.eql('/post');
+                done();
+            });
+
+            handleRouter({ pathname: '/post' }, {});
+        });
+
+        it('dynamic router str type', function (done) {
+            testTools();
+            router.add('/post/<str:title>', function (req, res, params) {
+                params.title.should.be.eql('demo');
+                done();
+            });
+
+            handleRouter({ pathname: '/post/demo' }, {});
+        });
+
+        it('dynamic router path type', function (done) {
+            testTools();
+            router.add('/post/<path:path>', function (req, res, params) {
+                params.path.should.be.eql('demo/dede');
+                done();
+            });
+
+            handleRouter({ pathname: '/post/demo/dede' }, {});
+        });
     });
 });
