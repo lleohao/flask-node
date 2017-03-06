0bd0902f894ab4a8706cd2a66af79a1a53d42020
hzzhouhao1 <hzzhouhao1@corp.netease.com>
2017-03-06 14:39:07 +0800

    refactor: change to ts

---
+++ lib/render.ts
@@ -0,0 +1,35 @@
+import * as swig from 'swig';
+import { join } from 'path';
+
+import { configs } from './flask';
+
+
+export function render() {
+    const debug = configs.runTimeOptions.debug;
+    const prefix = configs.flaskOptions.templatesPath;
+
+    if (debug) swig.setDefaults({ cache: false });
+
+    swig.setFilter('static', function (input) {
+        return './' + configs.flaskOptions.staticRootPath + '/' + input;
+    });
+
+    /**
+    * 封装 swig 操作，返回渲染后的模板
+    *
+    * @param templatePath 模板路径
+    * @param data 填充内容
+    * @returns {string} 渲染后的模板
+    */
+    return function (templatePath, data) {
+        let template;
+        if (/\.html$/.test(templatePath)) {
+            let realPath = join(prefix, templatePath);
+            template = swig.compileFile(realPath);
+        } else {
+            template = swig.compile(templatePath);
+        }
+
+        return template(data);
+    };
+}
