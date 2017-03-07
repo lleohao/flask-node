938889452484effc458720b4093ac129d2ac5634
hzzhouhao1 <hzzhouhao1@corp.netease.com>
2017-03-07 16:31:23 +0800

    feat(response): ok

--- lib/render.ts
+++ lib/render.ts
@@ -4,32 +4,30 @@ import { join } from 'path';
 import { configs } from './flask';
 
 
-export function render() {
-    const debug = configs.runTimeOptions.debug;
-    const prefix = configs.flaskOptions.templatesPath;
+const debug = configs.runTimeOptions.debug;
+const prefix = configs.flaskOptions.templatesPath;
 
-    if (debug) swig.setDefaults({ cache: false });
+if (debug) swig.setDefaults({ cache: false });
 
-    swig.setFilter('static', function (input) {
-        return './' + configs.flaskOptions.staticRootPath + '/' + input;
-    });
+swig.setFilter('static', function (input) {
+    return './' + configs.flaskOptions.staticRootPath + '/' + input;
+});
 
-    /**
-    * 封装 swig 操作，返回渲染后的模板
-    *
-    * @param templatePath 模板路径
-    * @param data 填充内容
-    * @returns {string} 渲染后的模板
-    */
-    return function (templatePath, data) {
-        let template;
-        if (/\.html$/.test(templatePath)) {
-            let realPath = join(prefix, templatePath);
-            template = swig.compileFile(realPath);
-        } else {
-            template = swig.compile(templatePath);
-        }
+/**
+* 封装 swig 操作，返回渲染后的模板
+*
+* @param {string} templatePath 模板路径
+* @param {Object} [data] 填充内容
+* @returns {string} 渲染后的模板
+*/
+export function render(templatePath: string, data?: Object): string {
+    let template;
+    if (/\.html$/.test(templatePath)) {
+        let realPath = join(prefix, templatePath);
+        template = swig.compileFile(realPath);
+    } else {
+        template = swig.compile(templatePath);
+    }
 
-        return template(data);
-    };
-}
+    return template(data);
+};
--- lib/response.ts
+++ lib/response.ts
@@ -1,11 +1,25 @@
 import { ServerResponse } from 'http';
 
+import { render } from './render';
+
 export interface ResponseData {
-    response: string;
+    entiry: string;
     status: number;
     headers: Object
 }
 
+interface CookieValue {
+    key: string;
+    value: string;
+    maxAge?: number;
+    expires?: number;
+    domain?: string;
+    path?: string;
+}
+
+const defaultHeader = {
+    'Content-Type': 'text/html'
+}
 
 export class Response {
     private res: ServerResponse;
@@ -14,23 +28,61 @@ export class Response {
         this.res = res;
     }
 
-    private finish(status, headers, data) {
+    private finish(status: number, headers: Object, entiry?: Object) {
+        let res = this.res;
 
+        headers = Object.assign(defaultHeader, headers);
+
+        res.writeHead(status, headers);
+        if (entiry !== undefined) res.write(entiry);
+
+        res.end();
     }
 
-    setCookie() {
+    setCookie(...cookies: CookieValue[]) {
+        cookies.forEach((data) => {
+            let cookie = `${data.key}=${data.value}; `;
 
+            if (data.maxAge !== undefined) cookie += `Max-Age = ${data.maxAge}; `;
+            if (data.domain !== undefined) cookie += `Domain  = ${data.domain}; `;
+            if (data.expires !== undefined) cookie += `Expires = ${data.expires}; `;
+            if (data.path !== undefined) cookie += `Path    = ${data.path}; `;
+
+            this.res.setHeader('Set-Cookie', cookie);
+        })
     }
 
-    end(response: string | ResponseData, status?: number) {
-        this.finish(status, (<ResponseData>response).headers, (<ResponseData>response).headers);
-    }
-
-    abort() {
-
+    abort(status = 401) {
+        this.finish(status, {});
     }
 
     redirect(url: string) {
-        
+        let status = 301;
+        let headers = {
+            'Location': url
+        }
+
+        this.finish(status, headers);
+    }
+
+    render(templatePath: string, data?: Object) {
+        let response = render(templatePath, data);
+
+        this.end(response);
+    }
+
+    end(response: string | ResponseData, status: number = 200) {
+        let entiry;
+        let headers = {};
+
+        if (typeof response === 'string') {
+            entiry = response;
+        } else {
+            entiry = response.entiry;
+            headers = response.headers;
+            status = response.status;
+        }
+
+        this.finish(status, headers, entiry);
     }
 }
