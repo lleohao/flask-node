7cbf409daf26c07a013feecfe62e28b2d2c68dd9
lleohao <lleohao@hotmail.com>
2017-05-10 14:55:42 +0800

    test: add render test

---
+++ test/render.test.ts
@@ -0,0 +1,23 @@
+/// <reference path="../node_modules/@types/mocha/index.d.ts" />
+/// <reference path="../node_modules/@types/node/index.d.ts" />
+/// <reference path="../node_modules/@types/should/index.d.ts" />
+
+import * as should from 'should';
+import { Configs } from '../lib/configs';
+import { render } from '../lib/render';
+
+describe('render test', () => {
+    it('return string template', () => {
+        const template = '<h1>hello world</h1>';
+
+        should(render(template)).be.eql(template);
+    })
+
+    it('return string template with data', () => {
+        const template = '<h1>hello {{ name }}</h1>';
+
+        should(render(template, {
+            name: 'lleohao'
+        })).be.eql('<h1>hello lleohao</h1>');
+    });
+});
