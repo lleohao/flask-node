f1621e8a39e6ec8a2e797e75767f9b1c6023dd58
lleohao <lleohao@hotmail.com>
2017-05-12 23:30:33 +0800

    perf: add login demo

--- example/app.ts
+++ example/app.ts
@@ -4,10 +4,47 @@ import { Flask, Router, Request, Response } from '../lib';
 const app = new Flask(join(__dirname, '..', 'example'));
 const router = new Router();
 
+let isLogin = false;
+
 const indexHandle = (req: Request, res: Response) => {
-    res.render('index.html');
+    if (isLogin === false) {
+        res.redirect('/login');
+    } else {
+        res.render('index.html');
+    }
+};
+
+const uploadHandle = (req: Request, res: Response) => {
+    if (isLogin === false) {
+        res.redirect('/login');
+    } else {
+        res.render('upload-main.html');
+    }
+};
+
+const loginHandle = (req: Request, res: Response) => {
+    const user = {
+        username: 'admin@admin.com',
+        password: 'root'
+    };
+
+    if (req.method === 'get') {
+        res.render('login-main.html');
+    } else {
+        if (req.form('username') === user.username
+            && req.form('password') === user.password) {
+            isLogin = true;
+            res.redirect('/');
+        } else {
+            res.render('login-main.html', {
+                message: '账户或密码错误'
+            });
+        }
+    }
 };
 
 router.add('/', ['get'], indexHandle);
+router.add('/upload', ['get'], uploadHandle);
+router.add('/login', ['get', 'post'], loginHandle);
 
 app.run({ debug: true });
