4a8265035eb9ab8394dfec5b58d4c34cb89f5b6f
lleohao <lleohao@hotmail.com>
2017-05-12 10:25:52 +0800

    chore: remove log

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,7 +11,6 @@ export function handleRouter(req: Request, res: Response) {
         len = urlMap.length;
     for (; i < len; i++) {
         let result = urlMap[i].match(req.pathname);
-        console.log(urlMap[i]);
         if (result) {
             let endpointName = urlMap[i].endpoint;
 
